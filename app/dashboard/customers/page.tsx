282b942edd21fa86ad021f516976ef9d20efb745
“techxxy” <jeeyoung.park.5@gmail.com>
2024-03-08 02:40:41 +0100

    migrate to prisma

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,9 +1,29 @@
 import { Metadata } from 'next';
+import { PrismaClient } from '@prisma/client'
  
 export const metadata: Metadata = {
   title: 'Customer',
 };
 
 export default function Page (){
-    return <p>Customers Page</p>;
-} 
+
+
+  return <p>Customers Page</p>;
+}
+const prisma = new PrismaClient()
+
+async function main() {
+  const allUsers = await prisma.users.findMany()
+  console.log(allUsers)
+  // ... you will write your Prisma Client queries here
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect()
+  })
+  .catch(async (e) => {
+    console.error(e)
+    await prisma.$disconnect()
+    process.exit(1)
+  })
