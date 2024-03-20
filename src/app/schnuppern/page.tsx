d8820f8726763875cfa79edaf2dbcbe21345701e
“techxxy” <jeeyoung.park.5@gmail.com>
2024-03-20 13:38:40 +0100

    apply h1 style

--- src/app/schnuppern/page.tsx
+++ src/app/schnuppern/page.tsx
@@ -6,19 +6,20 @@ import { Boundary } from '@/components/ui/boundary';
 const SchnuppernPage: React.FC = () => {
 
   return (
-     <div>
-          <Boundary
-        labels={['Korean']}
+     <>
+      <h1 className='mb-4 text-3xl font-extrabold text-gray-900 dark:text-white md:text-5xl lg:text-6xl text-transparent bg-clip-text bg-gradient-to-r to-emerald-600 from-sky-400'>Koreanisches Alphabet</h1>
+        <Boundary
+        labels={['Koreanisches Alphabet']}
         color="default"
         size="small"
         animateRerendering={true}
       >
-      <p className='text-gray-1000'>Lassen Sie uns deutsche Wörter so schreiben, wie sie klingen, auf Koreanisch.</p>
-      <p className='text-gray-1000'>Koreanisch Schreiben, Hangul ist ein Alphabet, das Laute darstellt. </p>
+      <p className='text-gray-1000'>Schreiben Sie deutsche Wörter auf Koreanisch Schrift, Hangul.</p>
+      <p className='text-gray-1000'>Hangul ist wie deutsch Alphabet, das Laute darstellt. </p>
       <p className='text-gray-1000'>Ein Hangul-Zeichen entspricht einer Silbe. </p>
       </Boundary>
         <SchnuppernTyping />       
-      </div>
+      </>
   );
 };
 
