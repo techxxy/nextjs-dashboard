11bb34fb46f36feaf844fac7b49f8c50ba1e99c2
“techxxy” <jeeyoung.park.5@gmail.com>
2024-03-21 12:26:01 +0100

    add adress bar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,16 @@
 import '@/app/ui/global.css';
 import { inter } from '@/app/ui/fonts';
 import { Metadata } from 'next';
-import SideNav from "./ui/dashboard/sidenav";
+import SideNav from './ui/dashboard/sidenav';
+import { AddressBar } from '@/components/ui/address-bar';
 
 export const metadata: Metadata = {
   title: {
     template: 'Techxxy%s | Fire up Korean Alphabet',
     default: 'Koreanisch Lernen',
   },
-  description: 'Diese Webseite wurde für deutschsprachige Nutzer entwickelt, die Koreanisch als Anfänger lernen möchten. Wir verstehen, dass das Erlernen einer Fremdsprache eine Herausforderung darstellt, insbesondere für Erwachsene, die möglicherweise nicht die Zeit haben, wie Schüler viel Zeit für den Lernprozess aufzuwenden. Unsere Webseite bietet eine benutzerfreundliche und unterhaltsame Lernumgebung, die speziell darauf abzielt, deutschsprachigen Benutzern dabei zu helfen, Koreanisch effektiv und effizient zu erlernen. Tauchen Sie ein in eine Welt des Sprachenlernens, die auf Ihre Bedürfnisse zugeschnitten ist',
+  description:
+    'Diese Webseite wurde für deutschsprachige Nutzer entwickelt, die Koreanisch als Anfänger lernen möchten. Wir verstehen, dass das Erlernen einer Fremdsprache eine Herausforderung darstellt, insbesondere für Erwachsene, die möglicherweise nicht die Zeit haben, wie Schüler viel Zeit für den Lernprozess aufzuwenden. Unsere Webseite bietet eine benutzerfreundliche und unterhaltsame Lernumgebung, die speziell darauf abzielt, deutschsprachigen Benutzern dabei zu helfen, Koreanisch effektiv und effizient zu erlernen. Tauchen Sie ein in eine Welt des Sprachenlernens, die auf Ihre Bedürfnisse zugeschnitten ist',
   metadataBase: new URL('https://techxxy.github.io/'),
   openGraph: {
     title: 'Fire up Korean Alphabet',
@@ -16,11 +18,11 @@ export const metadata: Metadata = {
       'Plattform zum Lernen von Koreanisch für deutschsprachige Nutzer. Bietet eine unterhaltsame und effiziente Lernerfahrung.',
     // images: [`/api/og?title=Next.js App Router`],
   },
-/*   twitter: {
+  /*   twitter: {
     card: 'summary_large_image',
   }, */
 };
- 
+
 export default function RootLayout({
   children,
 }: {
@@ -29,12 +31,29 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${inter.className} antialiased`}>
-      <div className="flex h-screen flex-col lg:flex-row lg:overflow-hidden">
-        <div className="w-full flex-none lg:w-64">
-          <SideNav />
+        <div className="flex h-screen flex-col lg:flex-row lg:overflow-hidden">
+
+          <div className="w-full flex-none lg:w-64">
+            <SideNav />
+          </div>
+         
+          <div className="max-w-5xl space-y-8 px-2 pt-20 lg:py-8">
+
+          <div className="bg-vc-border-gradient rounded-lg p-px shadow-lg shadow-black/20">
+              <div className="rounded-lg bg-black">
+                <AddressBar />
+              </div>
+            </div>
+
+
+                <div className="flex-grow p-6 lg:overflow-y-auto lg:p-12">
+                  {children}
+                </div>
+          </div>
+
+
+
         </div>
-        <div className="flex-grow p-6 lg:overflow-y-auto lg:p-12">{children}</div>
-      </div>
       </body>
     </html>
   );
--- src/app/ui/dashboard/nav-links.tsx
+++ src/app/ui/dashboard/nav-links.tsx
@@ -9,16 +9,17 @@ import {
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 import clsx from 'clsx';
+import { menus } from '@/lib/menus';
 
 
 // Map of links to display in the side navigation.
 // Depending on the size of the application, this would be stored in a database.
 const links = [
-  { name: 'Schnuppern', href: '/schnuppern', icon: HomeIcon },
-  { name: 'Vocal', href: '/vowel', icon: LanguageIcon },
-  { name: 'Keyboard Example', href: '/alphabets', icon: LanguageIcon },
-  { name: 'PressCountAPI', href: '/sample', icon: HomeIcon },
-  { name: 'Home', href: '/alphabets', icon: HomeIcon },
+  { name: 'Schnuppern', href: '/schnuppern'},
+  { name: 'Vocal', href: '/vowel'},
+  { name: 'Keyboard Example', href: '/alphabets'},
+  { name: 'PressCountAPI', href: '/sample'},
+  { name: 'Home', href: '/alphabets'},
 ];
 
 export default function NavLinks() {
@@ -27,18 +28,16 @@ export default function NavLinks() {
   return (
     <>
       {links.map((link) => {
-        const LinkIcon = link.icon;
         return (
           <Link
             key={link.name}
             href={link.href}
-            className={clsx("flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 lg:flex-none lg:justify-start lg:p-2 lg:px-3",
+            className={clsx("flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 lg:flex-none lg:justify-start lg:p-2 lg:px-6",
             {
               'bg-sky-100 text-blue-600': pathname === link.href,
             },
             )}
             >
-            <LinkIcon className="w-6" />
             <p className="hidden md:block">{link.name}</p>
           </Link>
         );
