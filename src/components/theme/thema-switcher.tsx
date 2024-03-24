f91717ae1fc0fe85b602468218df1fa811c93ef1
“techxxy” <jeeyoung.park.5@gmail.com>
2024-03-24 14:56:06 +0100

    change moon icon and rotation

--- src/components/theme/thema-switcher.tsx
+++ src/components/theme/thema-switcher.tsx
@@ -1,30 +1,47 @@
-"use client";
+'use client';
 
 import { useTheme } from 'next-themes';
 import { SunIcon } from '@heroicons/react/24/outline';
 import { MoonIcon } from '@heroicons/react/24/solid';
+import { GoMoon } from 'react-icons/go';
+import { useState } from 'react';
 
 const ThemeSwitcher = () => {
   const { systemTheme, theme, setTheme } = useTheme();
 
   const renderThemeChanger = () => {
     const currentTheme = theme === 'system' ? systemTheme : theme;
+      const [isHovered, setIsHovered] = useState(false);
 
     if (currentTheme === 'dark') {
       return (
         <SunIcon
-          className="h-6 w-6 text-amber-500 transition duration-500 transform hover:rotate-45"
+          className="h-6 w-6 transform text-amber-500 transition duration-500 hover:rotate-45"
           role="button"
           onClick={() => setTheme('light')}
         />
       );
     } else {
       return (
-        <MoonIcon
+        <>
+    <GoMoon
+      className="h-6 w-6 text-gray-500"
+      style={{
+        transform: `rotate(${isHovered ? '15deg' : '-20deg'})`,
+        transition: 'transform 0.5s',
+        cursor: 'pointer' // Optionally, add cursor style for better UX
+      }}
+      role="button"
+      onMouseEnter={() => setIsHovered(true)}
+      onMouseLeave={() => setIsHovered(false)}
+      onClick={() => setTheme('dark')}
+    />
+          {/*         <MoonIcon
           className="h-6 w-6 text-gray-500 transition duration-500 transform hover:rotate-45"
           role="button"
           onClick={() => setTheme('dark')}
-        />
+        /> */}
+        </>
       );
     }
   };
