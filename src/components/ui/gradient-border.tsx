1d69bef9ba71cca04a0aea5f0718364ae140a65e
“techxxy” <jeeyoung.park.5@gmail.com>
2024-03-24 11:25:55 +0100

    improve the gradient border code

--- src/components/ui/gradient-border.tsx
+++ src/components/ui/gradient-border.tsx
@@ -1,12 +1,14 @@
 import React, { ReactNode } from 'react';
 
-interface GradientBorderProps {
+export default function GradientBorder ({
+  children,
+  className,
+  fullMode,
+}: {
   children: ReactNode;
   className?: string;
   fullMode?:'h-full';
-}
-
-const GradientBorder: React.FC<GradientBorderProps> = ({ children, className, fullMode }) => {
+}) {
   return (
     <div className={`rounded-lg bg-opacity-80 p-px shadow-lg dark:bg-vc-border-gradient ${className}`}>
       <div className={`rounded-lg dark:bg-black dark:bg-none ${fullMode}`}>
@@ -16,4 +18,3 @@ const GradientBorder: React.FC<GradientBorderProps> = ({ children, className, fu
   );
 };
 
-export default GradientBorder;
