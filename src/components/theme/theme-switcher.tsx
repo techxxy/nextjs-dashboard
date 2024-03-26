c34048ee11158f777f07f1090619ddc6379e8635
“techxxy” <jeeyoung.park.5@gmail.com>
2024-03-26 07:52:20 +0100

    remove unnecessasry renderThema function

--- src/components/theme/theme-switcher.tsx
+++ src/components/theme/theme-switcher.tsx
@@ -6,44 +6,35 @@ import { MoonIcon } from '@heroicons/react/24/solid';
 import { GoMoon } from 'react-icons/go';
 import { useState } from 'react';
 
-export default function ThemeSwitcher ({
-  className,
-}: {
-  className?: string;
-}) {
+export default function ThemeSwitcher({ className }: { className?: string }) {
   const { systemTheme, theme, setTheme } = useTheme();
+  const currentTheme = theme === 'system' ? systemTheme : theme;
+  const [isHovered, setIsHovered] = useState(false);
 
-  const renderThemeChanger = () => {
-    const currentTheme = theme === 'system' ? systemTheme : theme;
-      const [isHovered, setIsHovered] = useState(false);
-
-    if (currentTheme === 'dark') {
-      return (
-        <SunIcon
-          className={`h-6 w-6 transform text-amber-500 transition duration-500 hover:rotate-45 ${className}`}
+  if (currentTheme === 'dark') {
+    return (
+      <SunIcon
+        className={`h-6 w-6 transform text-amber-500 transition duration-500 hover:rotate-45 ${className}`}
+        role="button"
+        onClick={() => setTheme('light')}
+      />
+    );
+  } else {
+    return (
+      <>
+        <GoMoon
+          className={`h-6 w-6 text-gray-500 ${className}`}
+          style={{
+            transform: `rotate(${isHovered ? '15deg' : '-20deg'})`,
+            transition: 'transform 0.5s',
+            cursor: 'pointer', // Optionally, add cursor style for better UX
+          }}
           role="button"
-          onClick={() => setTheme('light')}
+          onMouseEnter={() => setIsHovered(true)}
+          onMouseLeave={() => setIsHovered(false)}
+          onClick={() => setTheme('dark')}
         />
-      );
-    } else {
-      return (
-        <>
-    <GoMoon
-      className={`h-6 w-6 text-gray-500 ${className}`}
-      style={{
-        transform: `rotate(${isHovered ? '15deg' : '-20deg'})`,
-        transition: 'transform 0.5s',
-        cursor: 'pointer' // Optionally, add cursor style for better UX
-      }}
-      role="button"
-      onMouseEnter={() => setIsHovered(true)}
-      onMouseLeave={() => setIsHovered(false)}
-      onClick={() => setTheme('dark')}
-    />
-        </>
-      );
-    }
-  };
-
-  return <>{renderThemeChanger()}</>;
-};
+      </>
+    );
+  }
+}
